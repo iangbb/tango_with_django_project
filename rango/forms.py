70c34c4633dea7aca288952f43a3bd595e761c63
Ian Ballingall <2319170B@student.gla.ac.uk>
2019-02-08 14:29:01 +0000

    Ch9: user registration and authentication

--- rango/forms.py
+++ rango/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from rango.models import Page, Category
+from rango.models import Page, Category, UserProfile
+from django.contrib.auth.models import User
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=Category._meta.get_field('name').max_length,
@@ -33,3 +34,14 @@ class PageForm(forms.ModelForm):
         model = Page
         exclude = ('category',)
 
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture')
